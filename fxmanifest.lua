de0c4786d0f4bcb800efea519c61027b1e4224c9
Belga Mods <didier1998@live.be>
2024-03-21 17:51:02 +0100

    update fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+author 'mraes <all rights to him>'
+discription 'A update version about the heliscript form mreas' 
+version '1.3.1'
+
+
+client_scripts {
+    'heli_client.lua'
+}
+server_script 'heli_server.lua'
+
